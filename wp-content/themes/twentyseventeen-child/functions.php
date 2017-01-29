cb7b0f7a88c5870eacf5e138b2759ba12bf6bc8c
Fenn Macon <fennmacon@gmail.com>
2017-01-29 18:58:16 -0500

    Delete functions.php

--- wp-content/themes/twentyseventeen-child/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_action( 'init', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-	wp_enqueue_style(	'parent-style', get_template_directory_uri() . '/style.css'	);
-	wp_enqueue_style(	'main-style', get_stylesheet_directory_uri() . '/style.css'	);
-	wp_enqueue_scripts( 'main-js', get_stylesheet_directory_uri() . '/scripts/scripts.js');
-}
-?>
